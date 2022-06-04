790b7a2b7fd411156fd1dbb27dfd05987cd81a99
sajad-ullattupara <106068240+sajad-ullattupara@users.noreply.github.com>
2022-06-04 18:28:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2377.1049029470732!2d-2.963018684501543!3d53.430832576081116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b21654b02538b%3A0x84576a57e21973ff!2sAnfield!5e0!3m2!1sen!2sin!4v1654346788946!5m2!1sen!2sin"
+        width="500"
+        height="400"
+        style={{ border: "0", margin: "50px"}}     
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
